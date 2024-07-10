9587832d56b2a7409d8523a271d91a2a133c8375
assylzhana <kabibullaasylzhan@gmail.com>
2024-07-10 11:57:43 +0500

    adding api-gateway

--- order-service/src/main/java/com/shop/order_service/service/OrderService.java
+++ order-service/src/main/java/com/shop/order_service/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -39,8 +39,8 @@ public class OrderService {
                 .toList();
         //hereeeeeeee!!!
         //InventoryService suda , i dobavlyaem zakaz esli product is in
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
